09414a43eaeede5dbff66a865f7d1ff9ed6130d4
Muhamad Taopik <mr.94t3z@gmail.com>
2026-08-14 18:27:20 +0700

    Remove invalid extension module preloads

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import { resolve } from "node:path";
 
 export default defineConfig({
+  base: "/",
   build: {
     outDir: "dist",
     emptyOutDir: true,
+    modulePreload: false,
     sourcemap: true,
     rollupOptions: {
       input: {
